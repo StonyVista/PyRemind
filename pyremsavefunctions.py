24bbc0efc84016b5b835a6873ec465ba37141747
SVista <37023232+StonyVista@users.noreply.github.com>
2019-09-07 12:11:54 +0100

    Save functuins now working

--- pyremsavefunctions.py
+++ pyremsavefunctions.py
@@ -2,27 +2,36 @@ print('Save functions library started')
 import os
 
 #load editable non-constant data from the file
-def loadSaveFromDate(date):
-	date = date.split('/')
+def loadSaveFromDate(datestr):
+	date = datestr.split('/')
 	f = open(date[2]+'.txt')
 	yearsdata = f.readlines()
 	f.close
-	entry == 0
-	while eventtime != date.rstrip():
+	entry = 0
+	eventtime = 0
+	while eventtime != datestr:
+		#print(entry)
 		event = yearsdata[entry]
 		event = event.split(';')
 		eventtime = event[0]
 		entry += 1
-	return event
+	return event[1].rstrip()
 
 #save the data to a file for the editable non-constant
-def saveDataToDate(data,date()):
+def saveDataToDate(data,date):
 	year = date.split('/')[2]
+	if os.path.exists(year+'.txt') == False:
+		f = open(year+'.txt','w')
+		#f.write('')
+		f.close()
 	f = open(year+'.txt','r')
 	file = f.readlines()
 	f.close()
 	os.remove(year+'.txt')
-	file.append(data+';'+data)
-	f = open(year+'.txt',w)
-	f.write(file)
+	file.append(date+';'+data)
+	f = open(year+'.txt','w')
+	for a in file:
+		f.write(a)
+		f.write('\n')
 	f.close()
+	
